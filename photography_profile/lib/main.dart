9ffc642dca180b59bc5e0eabeaa33fc9b738e8a3
KevKipkemoi <kevkipkemoi@outlook.com>
2020-11-18 23:31:12 +0300

    follow section

--- photography_profile/lib/main.dart
+++ photography_profile/lib/main.dart
@@ -61,28 +61,108 @@ class MyHomePage extends StatelessWidget {
                         color: Colors.white,
                       ),
                     ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.location_on,
-                          color: Colors.white,
-                          size: 17,
-                        ),
-                        Text(
-                          'Nairobi - KE',
-                          style: TextStyle(
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.location_on,
                             color: Colors.white,
-                            wordSpacing: 2,
-                            letterSpacing: 4,
+                            size: 17,
                           ),
-                        ),
-                      ],
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(
+                              'Nairobi - KE',
+                              style: TextStyle(
+                                color: Colors.white,
+                                wordSpacing: 2,
+                                letterSpacing: 4,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     )
                   ],
                 ),
               ),
             ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 38, left: 38, top: 15, bottom: 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      '5K',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
+                    ),
+                    Text(
+                      'Followers',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Container(
+                      color: Colors.white,
+                      width: 0.2,
+                      height: 22,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '287',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          ),
+                        ),
+                        Text(
+                          'Following',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ]
+                    ),
+                    Container(
+                      color: Colors.white,
+                      width: 0.2,
+                      height: 22,
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 18, right: 18, top: 8, bottom: 8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(33)),
+                        gradient: LinearGradient(
+                          colors: [Color(0xff6D0EB3), Color(0xff4059F1)],
+                          begin: Alignment.bottomRight,
+                          end: Alignment.centerLeft,
+                        )
+                      ),
+                      child: Text(
+                        'Follow',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
