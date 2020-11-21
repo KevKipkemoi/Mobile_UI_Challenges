c3e4ae6798269de127492bf4221679be23444629
KevKipkemoi <kevkipkemoi@outlook.com>
2020-11-21 22:47:44 +0300

    initial commit

---
+++ bank_app/lib/screens/TransactionsScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TransactionsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[]
+    );
+  }
+}
