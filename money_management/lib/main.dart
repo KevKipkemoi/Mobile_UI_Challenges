78a21897e3113e71dbd28ca50bd101bdb93f4bca
KevKipkemoi <kevkipkemoi@outlook.com>
2020-12-03 23:28:19 +0300

    customShapeClipper

--- money_management/lib/main.dart
+++ money_management/lib/main.dart
@@ -46,29 +46,114 @@ class _MyHomePageState extends State<MyHomePage> {
                 border: Border.all(color: primaryColor),
               ),
               child: Padding(
-                padding: EdgeInsets.only(top: 30, right: 15, left: 15),
+                padding: EdgeInsets.only(top: 30.0, right: 15.0, left: 15.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     IconButton(
                       icon: Icon(Icons.menu),
                       color: Colors.white,
-                      iconSize: 30,
+                      iconSize: 30.0,
                       onPressed: (){},
                     ),
                     IconButton(
                       icon: Icon(Icons.notifications_none),
                       color: Colors.white,
-                      iconSize: 30,
+                      iconSize: 30.0,
                       onPressed: (){},
-                    )
+                    ),
                   ],
                 ),
               ),
+            ),
+            Stack(
+              children: <Widget>[
+                ClipPath(
+                  clipper: CustomShapeClipper(),
+                  child: Container(
+                    height: 350.0,
+                    decoration: BoxDecoration(color: primaryColor),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            '280.00',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 15.0),
+                          Text(
+                            'Available Balance',
+                            style: TextStyle(color: Colors.white, fontSize: 14.0),
+                          ),
+                        ],
+                      ),
+                      Material(
+                        elevation: 1,
+                        borderRadius: BorderRadius.circular(100.0),
+                        color: Colors.blue[300],
+                        child: MaterialButton(
+                          onPressed: () {},
+                          padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                          child: Text(
+                            'TOP UP',
+                            style: TextStyle(fontSize: 16.0, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 120.0, right: 25.0, left: 25.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 370,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          offset: Offset(0.0, 3.0),
+                          blurRadius: 15.0,
+                        ),
+                      ]
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
     );
   }
+}
+
+class CustomShapeClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+
+    path.lineTo(0.0, 390.0 - 200.0);
+    path.quadraticBezierTo(size.width / 2.0, 280.0, size.width, 390.0 - 200.0);
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path; 
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true; 
 }
